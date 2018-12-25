3a35f16aa1a2bcf1c5244798c4e8061b440fccf4
Rahul <Rahul@Rahul-PC>
2018-12-25 20:43:59 +0530

    Added Dynamic Dropdowns

---
+++ Selenium_3.141.59_Dec2018/src/com/boyzone/app/Junit/DynamicDropDowns.java
@@ -0,0 +1,44 @@
+package com.boyzone.app.Junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DynamicDropDowns {
+	
+	WebDriver driver;
+
+	@Before
+	public void setUp() throws Exception {
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		
+	}
+	
+	@Test
+	public void test() throws InterruptedException {
+		driver.get("https://www.spicejet.com/");
+		//Selected Source
+		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//a[@value='BLR']")).click();
+		Thread.sleep(2000);
+		
+		//Select Destination (Handled Dynamic Dropdown)
+		
+		driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	
+
+}
