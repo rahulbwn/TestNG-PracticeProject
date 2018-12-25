e94ead0434925225393c058a08aa535d1116eb1c
Rahul <Rahul@Rahul-PC>
2018-12-26 00:52:15 +0530

    test

--- Selenium_3.141.59_Dec2018/src/com/boyzone/app/Junit/DynamicDropDowns.java
+++ Selenium_3.141.59_Dec2018/src/com/boyzone/app/Junit/DynamicDropDowns.java
@@ -26,12 +26,22 @@ public class DynamicDropDowns {
 		//Selected Source
 		driver.findElement(By.id("ctl00_mainContent_ddl_originStation1_CTXT")).click();
 		Thread.sleep(2000);
-		driver.findElement(By.xpath("//a[@value='BLR']")).click();
+		
+	/*	driver.findElement(By.xpath("//a[@value='BLR']")).click();
 		Thread.sleep(2000);
 		
 		//Select Destination (Handled Dynamic Dropdown)
 		
-		driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();
+		driver.findElement(By.xpath("(//a[@value='MAA'])[2]")).click();*/
+		
+		//Alternate Way (Best Way)
+		driver.findElement(By.xpath("//div[@id='ctl00_mainContent_ddl_originStation1_CTNR']//a[@value='BLR']")).click();
+		
+		Thread.sleep(2000);
+		
+		// Select Destination
+		
+		driver.findElement(By.xpath("//div[@id='glsctl00_mainContent_ddl_destinationStation1_CTNR']//a[@value='MAA']")).click();
 		
 	}
 
