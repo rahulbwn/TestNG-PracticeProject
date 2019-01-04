f012c488a317706c651099305e53f79d52061b76
rahulbwn <rahulbwn@users.noreply.github.com>
2019-01-04 16:33:36 -0600

    Update StateofElement.java

--- Selenium_3.141.59_Dec2018/src/com/boyzone/app/Junit/StateofElement.java
+++ Selenium_3.141.59_Dec2018/src/com/boyzone/app/Junit/StateofElement.java
@@ -1,21 +1,24 @@
 package com.boyzone.app.Junit;
 
 import com.boyzone.app.genericMethod.GenericMethod;
-
+import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class StateofElement {
 	
 	WebDriver driver;
 	boolean ElementState;
+	boolean ElePresent;
 	GenericMethod gm;
 
 	@Before
 	public void setUp() throws Exception {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\Rahul\\workspace\\chromedriver_new\\chromedriver.exe");
 		//System.setProperty("webdriver.chrome.driver", "C:\\\\Users\\\\Rahul\\\\eclipse-workspace\\\\chromedriver_win32\\\\chromedriver.exe");
 		driver=new ChromeDriver();
 		driver.manage().window().maximize();
@@ -29,8 +32,26 @@ public class StateofElement {
 	public void test()  {
 		driver.get("https://www.google.com");
 		//ElementState=driver.findElement(By.xpath("//input[@title='Search']")).isEnabled();
-		ElementState=gm.getElement("//input[@title='Search']", "XPAT").isEnabled();
-		System.out.println(ElementState);
+		ElementState=gm.getElement("//input[@title='Search']", "XPATH").isEnabled();
+		System.out.println("Is Element Enabled? "+ElementState);
+		
+		System.out.println("------------------------------------------------------------");
+		
+		List<WebElement> ListEle=gm.getElementList("//input[@type='submit']", "Xpath");
+		for(WebElement opt:ListEle)
+		{
+			System.out.println(opt.getText());
+		}
+		
+		System.out.println("------------------------------------------------------------");
+		
+		//When correct attribute value is passed
+		ElePresent=gm.isElementPresent("//input[@type='submit']", "Xpath");
+		System.out.println("Is Element Present? "+ElePresent);	
+		
+		//When Incorrect attribute value is passed
+				ElePresent=gm.isElementPresent("//input[@type='submit-text']", "Xpath");
+				System.out.println("Is Element Present? "+ElePresent);	
 	}
 
 	
