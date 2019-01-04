03490767f482e9adda8470d01e3f5d3f8a626310
rahulbwn <rahulbwn@users.noreply.github.com>
2019-01-04 15:45:26 -0600

    Update GenericMethod.java

--- Selenium_3.141.59_Dec2018/src/com/boyzone/app/genericMethod/GenericMethod.java
+++ Selenium_3.141.59_Dec2018/src/com/boyzone/app/genericMethod/GenericMethod.java
@@ -1,12 +1,15 @@
 package com.boyzone.app.genericMethod;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 public class GenericMethod {
 	 WebDriver driver;
-	WebElement element;
+	
 	public GenericMethod(WebDriver driver)
 	{
 		this.driver=driver;
@@ -14,7 +17,7 @@ public class GenericMethod {
 	
 	public WebElement getElement(String locator, String type) 
 	{
-		
+		WebElement element=null;
 		type=type.toLowerCase();
 		
 		if(type.equals("id"))
@@ -55,4 +58,57 @@ public class GenericMethod {
 		return element;
 		
 	}
+	
+	public List<WebElement> getElementList(String locator,String type)
+	{
+		List<WebElement> element=new ArrayList<WebElement>();
+		type=type.toLowerCase();
+		if(type.equals("id"))
+		{
+			System.out.println("Element id is having locator: "+locator);
+			element= this.driver.findElements(By.id(locator));
+		}
+		
+		else if(type.equals("xpath"))
+		{
+			System.out.println("Element xpath is having locator: "+locator);
+			element= this.driver.findElements(By.xpath(locator));
+		}
+		
+		else if(type.equals("cssselector"))
+		{
+			System.out.println("Element cssselector is having locator: "+locator);
+			element= this.driver.findElements(By.cssSelector(locator));
+		}
+		
+		else if(type.equals("linktext"))
+		{
+			System.out.println("Element linktext is having locator: "+locator);
+			element= this.driver.findElements(By.linkText(locator));
+		}
+		
+		else if(type.equals("partiallinktext"))
+		{
+			System.out.println("Element partiallinktext is having locator: "+locator);
+			element= this.driver.findElements(By.partialLinkText(locator));
+		}
+		
+		else
+		{
+			System.out.println("Locator Type not Supported: "+type+" Locator: "+locator);
+		}
+		
+		return element;
+		
 	}
+
+	public boolean isElementPresent(String locator, String type)
+	{
+		List<WebElement> eleList=getElementList(locator, type);
+				int size=eleList.size();
+				if(size>0)
+					return true;
+				else
+					return false;
+	}
+}
