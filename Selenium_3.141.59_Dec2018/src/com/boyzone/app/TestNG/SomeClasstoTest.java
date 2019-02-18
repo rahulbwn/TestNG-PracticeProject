bb7100e324e9701aaec4c5758d10cfa72d1bfa63
rahulbwn <rahulbwn@users.noreply.github.com>
2019-02-18 17:12:33 -0600

    Create SomeClasstoTest.java

---
+++ Selenium_3.141.59_Dec2018/src/com/boyzone/app/TestNG/SomeClasstoTest.java
@@ -0,0 +1,22 @@
+package TestN;
+
+public class SomeClasstoTest {
+
+	public int sum(int a,int b)
+	{
+		return a+b;
+	}
+	
+	public String ConcatString(String x,String y)
+	{
+		return x+" "+y;
+	}
+	
+	public int[] SampleArray()
+	{
+		int[] arr={1,2,3,4};
+		return arr;
+	}
+
+
+}
