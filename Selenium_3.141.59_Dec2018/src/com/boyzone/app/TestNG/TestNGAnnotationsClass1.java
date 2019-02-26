6ac76c937746760ed622f790161440cee9ea9450
rahulbwn <rahulbwn@users.noreply.github.com>
2019-02-26 10:40:52 -0600

    Create TestNGAnnotationsClass1.java

---
+++ Selenium_3.141.59_Dec2018/src/com/boyzone/app/TestNG/TestNGAnnotationsClass1.java
@@ -0,0 +1,41 @@
+package TestN;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import base.BaseSuiteClass;
+
+public class TestNGAnnotationsClass1 extends BaseSuiteClass {
+  @Test
+  public void test1() {
+	  System.out.println("Class 1 -> test1 execution");
+  }
+  
+  @Test
+  public void test2() {
+	  System.out.println("Class 1 -> test2 execution");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("Class 1 -> Before each Test Method");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("Class 1 -> After each Test Method");
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+	  System.out.println("Class 1 -> Before Class");
+  }
+
+  @AfterClass
+  public void afterClass() {
+	  System.out.println("Class 1 -> After Class");
+  }
+
+}
