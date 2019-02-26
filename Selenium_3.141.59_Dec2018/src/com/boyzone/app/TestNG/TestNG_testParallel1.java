635ba327fde2c09fed22f92cd3d259298c5474c2
rahulbwn <rahulbwn@users.noreply.github.com>
2019-02-26 17:33:07 -0600

    Create TestNG_testParallel1.java

---
+++ Selenium_3.141.59_Dec2018/src/com/boyzone/app/TestNG/TestNG_testParallel1.java
@@ -0,0 +1,19 @@
+package TestN;
+
+import org.testng.annotations.Test;
+
+public class TestNG_testParallel1 {
+  @Test
+  public void testMethod1() throws InterruptedException {
+	  System.out.println("TestNG_testParallel1 - > test method 1");
+	  Thread.sleep(6000);
+	  System.out.println("TestNG_testParallel1 - > test method 1 -> More steps");
+  }
+  
+  @Test
+  public void testMethod2() throws InterruptedException {
+	  System.out.println("TestNG_testParallel1 - > test method 2");
+	  Thread.sleep(6000);
+	  System.out.println("TestNG_testParallel1 - > test method 2 -> More steps");
+  }
+}
