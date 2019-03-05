02759fa8585caeef29bfca8af9ea2181fd3483c1
rahulbwn <rahulbwn@users.noreply.github.com>
2019-03-05 16:02:31 -0600

    Create CustomListener3.java

---
+++ Selenium_3.141.59_Dec2018/src/com/boyzone/app/com.TestngListeners/CustomListener3.java
@@ -0,0 +1,36 @@
+package com.TestngListeners;
+
+import org.testng.ISuite;
+import org.testng.ISuiteListener;
+
+public class CustomListener3 implements ISuiteListener {
+
+	@Override
+	public void onStart(ISuite arg0) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("onStart: Runs Before suit");
+		
+	}
+	
+	@Override
+	public void onFinish(ISuite arg0) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("onFinish: Runs After suit completion");
+		
+	}
+
+	
+	
+
+	
+	}
+
+	
+
+	
+
+
+
+	
