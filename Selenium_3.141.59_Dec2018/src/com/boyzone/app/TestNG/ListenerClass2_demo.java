72f44104c87859b2ee2400344410fccdffd64c7e
rahulbwn <rahulbwn@users.noreply.github.com>
2019-03-05 15:56:50 -0600

    Create ListenerClass2_demo.java

---
+++ Selenium_3.141.59_Dec2018/src/com/boyzone/app/TestNG/ListenerClass2_demo.java
@@ -0,0 +1,39 @@
+package TestN;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import com.TestngListeners.CustomListener2;
+
+
+//@Listeners(CustomListener2.class)
+public class ListenerClass2_demo {
+	
+	
+  
+  @BeforeClass
+  public void setUp() {
+	  System.out.println("Running before Class method");
+  }
+
+  @AfterClass
+  public void cleanUp() {
+	  System.out.println("Running after Class method");
+  }
+
+  
+  @Test
+  public void testMethod1() {
+	  System.out.println("Running test method 1");
+	  Assert.assertTrue(true);
+  }
+  
+  @Test
+  public void testMethod2() {
+	  System.out.println("Running test method 2");
+	  Assert.assertTrue(false);
+  }
+}
