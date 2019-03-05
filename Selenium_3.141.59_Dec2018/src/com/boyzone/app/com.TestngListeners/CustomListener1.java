b39103dbb06516a077fbe3092562653e7de9783a
rahulbwn <rahulbwn@users.noreply.github.com>
2019-03-05 16:00:56 -0600

    Create CustomListener1.java

---
+++ Selenium_3.141.59_Dec2018/src/com/boyzone/app/com.TestngListeners/CustomListener1.java
@@ -0,0 +1,28 @@
+package com.TestngListeners;
+
+import org.testng.IInvokedMethod;
+import org.testng.IInvokedMethodListener;
+import org.testng.ITestResult;
+
+public class CustomListener1 implements IInvokedMethodListener{
+
+	
+
+	@Override
+	public void beforeInvocation(IInvokedMethod arg0, ITestResult arg1) {
+		// Runs before every method in Test Class
+		
+		System.out.println("Before Invocation: "+arg1.getTestClass().getName()+" -> "+arg0.getTestMethod().getMethodName());
+		
+	}
+	
+	@Override
+	public void afterInvocation(IInvokedMethod arg0, ITestResult arg1) {
+		// Runs after every test method
+		System.out.println("After Invocation: "+arg1.getTestClass().getName()+" -> "+arg0.getTestMethod().getMethodName());
+		
+	}
+
+	
+
+}
