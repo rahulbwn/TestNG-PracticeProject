cc112a075cbaa5a71cbf9f55571df410f6e98819
rahulbwn <rahulbwn@users.noreply.github.com>
2019-03-14 18:12:44 -0500

    Create Constants.java

---
+++ Selenium_3.141.59_Dec2018/src/com/base/Constants.java
@@ -0,0 +1,7 @@
+package base;
+
+public class Constants {
+      public static final String URL = "http://letskodeit.com";
+      public static final String File_Path = "C:\\Users\\Rahul\\workspace\\TestNG1\\src\\TestN\\";
+      public static final String File_Name = "ExcelData.xlsx";
+}
